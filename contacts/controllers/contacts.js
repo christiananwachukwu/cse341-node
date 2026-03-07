b0793bbf0e7f154618e3ba95a1d3e8869b7cce8e
Christiana Nwachukwu <cnwachukwu4@byupathway.edu>
2026-03-08 00:43:14 +0100

    Add POST, PUT, DELETE endpoints and controllers

---
+++ contacts/controllers/contacts.js
@@ -0,0 +1,94 @@
+const { ObjectId } = require('mongodb');
+
+// GET all contacts
+const getAllContacts = async (req, res) => {
+  try {
+    const db = req.app.locals.db;
+    const contacts = await db.collection('contacts').find().toArray();
+    res.status(200).json(contacts);
+  } catch (err) {
+    res.status(500).json({ error: 'Something went wrong' });
+  }
+};
+
+// GET single contact by ID
+const getSingleContact = async (req, res) => {
+  try {
+    const db = req.app.locals.db;
+    const contact = await db.collection('contacts').findOne({
+      _id: new ObjectId(req.params.id)
+    });
+    if (!contact) {
+      return res.status(404).json({ error: 'Contact not found' });
+    }
+    res.status(200).json(contact);
+  } catch (err) {
+    res.status(500).json({ error: 'Something went wrong' });
+  }
+};
+
+// POST - create a new contact
+const createContact = async (req, res) => {
+  try {
+    const db = req.app.locals.db;
+    const contact = {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      favoriteColor: req.body.favoriteColor,
+      birthday: req.body.birthday
+    };
+    const result = await db.collection('contacts').insertOne(contact);
+    res.status(201).json({ id: result.insertedId });
+  } catch (err) {
+    res.status(500).json({ error: 'Something went wrong' });
+  }
+};
+
+// PUT - update a contact
+const updateContact = async (req, res) => {
+  try {
+    const db = req.app.locals.db;
+    const contact = {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      favoriteColor: req.body.favoriteColor,
+      birthday: req.body.birthday
+    };
+    const result = await db.collection('contacts').replaceOne(
+      { _id: new ObjectId(req.params.id) },
+      contact
+    );
+    if (result.matchedCount === 0) {
+      return res.status(404).json({ error: 'Contact not found' });
+    }
+    res.status(200).json({ message: 'Contact updated successfully' });
+  } catch (err) {
+    res.status(500).json({ error: 'Something went wrong' });
+  }
+};
+
+// DELETE - delete a contact
+const deleteContact = async (req, res) => {
+  try {
+    const db = req.app.locals.db;
+    const result = await db.collection('contacts').deleteOne({
+      _id: new ObjectId(req.params.id)
+    });
+    if (result.deletedCount === 0) {
+      return res.status(404).json({ error: 'Contact not found' });
+    }
+    res.status(200).json({ message: 'Contact deleted successfully' });
+  } catch (err) {
+    res.status(500).json({ error: 'Something went wrong' });
+  }
+};
+
+module.exports = {
+  getAllContacts,
+  getSingleContact,
+  createContact,
+  updateContact,
+  deleteContact
+};
--- contacts/routes/contacts.js
+++ contacts/routes/contacts.js
@@ -1,34 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { ObjectId } = require('mongodb');
+const contactsController = require('../controllers/contacts');
 
-// GET all contacts
-router.get('/contacts', async (req, res) => {
-  try {
-    const db = req.app.locals.db;
-    const contacts = await db.collection('contacts').find().toArray();
-    res.json(contacts);
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: 'Something went wrong' });
-  }
-});
-
-// GET single contact by ID
-router.get('/contacts/:id', async (req, res) => {
-  try {
-    const db = req.app.locals.db;
-    const contact = await db.collection('contacts').findOne({
-      _id: new ObjectId(req.params.id)
-    });
-    if (!contact) {
-      return res.status(404).json({ error: 'Contact not found' });
-    }
-    res.json(contact);
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: 'Something went wrong' });
-  }
-});
+router.get('/contacts', contactsController.getAllContacts);
+router.get('/contacts/:id', contactsController.getSingleContact);
+router.post('/contacts', contactsController.createContact);
+router.put('/contacts/:id', contactsController.updateContact);
+router.delete('/contacts/:id', contactsController.deleteContact);
 
 module.exports = router;
