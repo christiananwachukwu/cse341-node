2331970ff5ab51a142ddeac4bcac76dd3ee981d6
Christiana Nwachukwu <cnwachukwu4@byupathway.edu>
2026-03-09 21:52:17 +0100

    Add API Swagger Documentation

--- contacts/server.js
+++ contacts/server.js
@@ -1,10 +1,14 @@
 require('dotenv').config();
 const express = require('express');
 const { MongoClient } = require('mongodb');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument=require('./swagger.json');
+
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use('/', require('./routes/contacts'));
 
